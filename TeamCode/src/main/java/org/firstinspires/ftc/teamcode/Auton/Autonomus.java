18538869138653e55dca19696b6a7b3184de8d3e
ags3780 <hammerhead12as@icloud.com>
2024-01-03 19:04:31 -0600

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Autonomus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Autonomus.java
@@ -1,32 +1,15 @@
 package org.firstinspires.ftc.teamcode.Auton;
 
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous (name = "free")
+@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
 
 
 public class Autonomus extends LinearOpMode {
@@ -35,18 +18,12 @@ public class Autonomus extends LinearOpMode {
     private DcMotorEx wheelFR;
     private DcMotorEx wheelBL;
     private DcMotorEx wheelBR;
-    private DcMotorEx Viper;
-    private DcMotorEx in;
-    private DcMotorEx climb;
+
+    private ElapsedTime  runtime = new ElapsedTime();
 
 
-    private Servo flip;
-    //private DcMotorEx Insertnamehere
-    //private DcMotorEx Insertnamehere
-    private Servo drone;
-    private Servo claw;
-
-
+    static final double  FORWARD_SPEED = 0.6;
+    static final double     TURN_SPEED    = 0.5;
     @Override
     public void runOpMode() throws InterruptedException {
         //Motors
@@ -56,14 +33,7 @@ public class Autonomus extends LinearOpMode {
         wheelBR = hardwareMap.get(DcMotorEx.class, "wheelBR");
 
 
-        Viper = hardwareMap.get(DcMotorEx.class, "viper");
-        in = hardwareMap.get(DcMotorEx.class, "in");
-        climb = hardwareMap.get(DcMotorEx.class, "climb");
 
-        //------------SERVOS////
-        claw = hardwareMap.get(Servo.class, "claw");
-        flip = hardwareMap.get(Servo.class, "flip");
-        drone = hardwareMap.get(Servo.class, "drone");
 
         wheelFL.setDirection(DcMotorSimple.Direction.REVERSE);//REVERSE
         wheelFR.setDirection(DcMotorSimple.Direction.FORWARD);//FORWARD
@@ -72,63 +42,65 @@ public class Autonomus extends LinearOpMode {
 
 
         waitForStart();
+// Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
 
-        moveForward(1, 1000);
-        moveBackward(-1, 1000);
-    }
-    public void moveForward (int power, int time ){
-        wheelFL.setPower(power);
-        wheelBL.setPower(power);
-        wheelBR.setPower(power);
-        wheelBL.setPower(power);
-        sleep(time);
-        wheelFL.setPower(0);
-        wheelFR.setPower(0);
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
+
+        // Step 1:  Drive forward for 3 seconds
+        wheelFL.setPower(FORWARD_SPEED);
+        wheelFR.setPower(FORWARD_SPEED);
+        wheelBR.setPower(FORWARD_SPEED);
+        wheelBL.setPower(FORWARD_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 2:  Spin right for 1.3 seconds
+        wheelFL.setPower(TURN_SPEED);
+        wheelBL.setPower(TURN_SPEED);
+        wheelFR.setPower(-TURN_SPEED);
+        wheelBR.setPower(-TURN_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.3)) {
+            telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 3:  Drive Backward for 1 Second
+        wheelFL.setPower(-FORWARD_SPEED);
+        wheelBL.setPower(-FORWARD_SPEED);
+        wheelBR.setPower(-FORWARD_SPEED);
+        wheelFR.setPower(-FORWARD_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 4:  Stop
         wheelBL.setPower(0);
         wheelBR.setPower(0);
-
-
-    }
-    public  void  moveRight (int power, int time , int power2) {
-        wheelFL.setPower(power);
-        wheelFR.setPower(power2);
-        wheelBL.setPower(power);
-        wheelBR.setPower(power2);
-        sleep(time);
-        wheelFL.setPower(0);
         wheelFR.setPower(0);
-        wheelBL.setPower(0);
-        wheelBR.setPower(0);
-
-    }
-    public  void moveLeft (int power, int time , int power2) {
-        wheelFL.setPower(power2);
-        wheelFR.setPower(power);
-        wheelBL.setPower(power2);
-        wheelBR.setPower(power);
-        sleep(time);
         wheelFL.setPower(0);
-        wheelFR.setPower(0);
-        wheelBL.setPower(0);
-        wheelBR.setPower(0);
 
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
     }
-    public  void  moveBackward(int power, int time ) {
-        wheelFL.setPower(power);
-        wheelFR.setPower(power);
-        wheelBL.setPower(power);
-        wheelBR.setPower(power);
-        sleep(time);
-        wheelFL.setPower(0);
-        wheelFR.setPower(0);
-        wheelBL.setPower(0);
-        wheelBR.setPower(0);
-
-    }
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
